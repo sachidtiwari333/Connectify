56998d4a4cf72958127f675c032c7cc1f2bc48d2
Sachid Tiwari <sachidtiwari333@gmail.com>
2026-08-08 14:01:40 +0545

    Follow Bar functionality is added

--- client/src/components/FollowBar.jsx
+++ client/src/components/FollowBar.jsx
@@ -1,82 +1,81 @@
-
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
 const FollowBar = () => {
+  const [suggestionLoading, setSuggestionLoading] = useState(false);
+  const [suggestedUser, setSuggestedUser] = useState([]);
+  const fetchSuggestedUser = async () => {
+    if (suggestionLoading) return;
+    try {
+      setSuggestionLoading(true);
+      const response = await axios.get(
+        "http://localhost:3000/api/v1/user/suggested-user",
+        {
+          withCredentials: true,
+        },
+      );
+      console.log(response);
+      setSuggestedUser(response.data.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    fetchSuggestedUser();
+  }, []);
+  
+  const followhandler = async(userId) =>{
+    try{
+      const response = await axios.post(
+        `http://localhost:3000/api/v1/user/${userId}/follow`,
+        {},
+        {
+          withCredentials : true
+        }
+      )
+      console.log(response);
+      
+    }catch(err){
+      Error(err)
+    }
+  }
   return (
     <div className="flex flex-col gap-3 text-white  w-full">
-      <h1 className="text-xl lg:text-2xl font-bold">
-        Who to follow
-      </h1>
+      <h1 className="text-xl lg:text-2xl font-bold">Who to follow</h1>
 
-      <div className="flex items-center justify-between w-full gap-2">
-        <div className="flex gap-2 lg:gap-3 items-center min-w-0">
-          <img
-            src="https://img.magnific.com/premium-vector/business-man-avatar-profile_1133257-2431.jpg?semt=ais_hybrid&w=740&q=80"
-            alt=""
-            className="w-10 lg:w-12 h-10 lg:h-12 rounded-full shrink-0 object-cover"
-          />
+      {suggestedUser?.map((suggestion) => {
+        return (
+          <div
+            key={suggestion._id}
+            className="flex items-center justify-between w-full gap-2"
+          >
+            <div className="flex gap-2 lg:gap-3 items-center min-w-0">
+              {
+                suggestion.profileImage 
+                ? <img src={suggestion.profileImage} className='w-20 h-20 rounded-full' alt='Profile Image' />
+                : <h1 className='text-3xl font-bold'>{suggestion.fullname?.charAt(0) || "U"}</h1>
+              }
 
-          <div className="flex flex-col min-w-0">
-            <p className="text-sm lg:text-md truncate">
-              Full Name
-            </p>
-            <p className="text-xs lg:text-sm text-gray-400 truncate">
-              @username
-            </p>
+              <div className="flex flex-col min-w-0">
+                <p className="text-sm lg:text-md truncate">
+                  {suggestion?.fullname}
+                </p>
+                <p className="text-xs lg:text-sm text-gray-400 truncate">
+                  @{suggestion?.username}
+                </p>
+              </div>
+            </div>
+
+            <button onClick={()=>{
+              followhandler(suggestion._id)
+            }} className="px-3 lg:px-4 py-1.5 lg:py-2 bg-amber-500 rounded-2xl text-sm lg:text-base shrink-0 hover:bg-amber-600 active:scale-95">
+              Follow
+            </button>
           </div>
-        </div>
-
-        <button className="px-3 lg:px-4 py-1.5 lg:py-2 bg-amber-500 rounded-2xl text-sm lg:text-base shrink-0 hover:bg-amber-600 active:scale-95">
-          Follow
-        </button>
-      </div>
-
-      <div className="flex items-center justify-between w-full p-1 gap-2">
-        <div className="flex gap-2 lg:gap-3 items-center min-w-0">
-          <img
-            src="https://img.magnific.com/premium-vector/business-man-avatar-profile_1133257-2431.jpg?semt=ais_hybrid&w=740&q=80"
-            alt=""
-            className="w-10 lg:w-12 h-10 lg:h-12 rounded-full shrink-0 object-cover"
-          />
-
-          <div className="flex flex-col min-w-0">
-            <p className="text-sm lg:text-md truncate">
-              Full Name
-            </p>
-            <p className="text-xs lg:text-sm text-gray-400 truncate">
-              @username
-            </p>
-          </div>
-        </div>
-
-        <button className="px-3 lg:px-4 py-1.5 lg:py-2 bg-amber-500 rounded-2xl text-sm lg:text-base shrink-0 hover:bg-amber-600 active:scale-95">
-          Follow
-        </button>
-      </div>
-
-      <div className="flex items-center justify-between w-full p-1 gap-2">
-        <div className="flex gap-2 lg:gap-3 items-center min-w-0">
-          <img
-            src="https://img.magnific.com/premium-vector/business-man-avatar-profile_1133257-2431.jpg?semt=ais_hybrid&w=740&q=80"
-            alt=""
-            className="w-10 lg:w-12 h-10 lg:h-12 rounded-full shrink-0 object-cover"
-          />
-
-          <div className="flex flex-col min-w-0">
-            <p className="text-sm lg:text-md truncate">
-              Full Name
-            </p>
-            <p className="text-xs lg:text-sm text-gray-400 truncate">
-              @username
-            </p>
-          </div>
-        </div>
-
-        <button className="px-3 lg:px-4 py-1.5 lg:py-2 bg-amber-500 rounded-2xl text-sm lg:text-base shrink-0 hover:bg-amber-600 active:scale-95">
-          Follow
-        </button>
-      </div>
+        );
+      })}
     </div>
   );
 };
 
 export default FollowBar;
-
